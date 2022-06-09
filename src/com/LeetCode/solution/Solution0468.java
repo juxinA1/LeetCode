726e4d3cdbb9b70ff99aec41e13fd742fb09d283
PC470 <1129549361@qq.com>
2022-06-09 22:14:33 +0800

    468,699

---
+++ src/com/LeetCode/solution/Solution0468.java
@@ -0,0 +1,90 @@
+package com.LeetCode.solution;
+
+public class Solution0468 {
+	public String validIPAddress(String queryIP) {
+		
+		String[] ipArr = getIpType(queryIP);
+		if (ipArr.length == 4) {
+			if (checkIPv4(ipArr)) {
+				return "IPv4";
+			}
+		}
+		if (ipArr.length == 8) {
+			if (checkIPv6(ipArr)) {
+				return "IPv6";
+			}
+		}
+		return "Neither";
+	}
+
+	private String[] getIpType(String queryIP) {
+
+		// 验证是否时IPv4
+		String[] arrayIPv4 = queryIP.split("\\.", -1);
+		if (arrayIPv4.length == 4) {
+			return arrayIPv4;
+		}
+
+		// 验证是否时IPv6
+		String[] arrayIPv6 = queryIP.split("\\:", -1);
+		if (arrayIPv6.length == 8) {
+			return arrayIPv6;
+		}
+
+		return new String[0];
+	}
+
+	/**
+	 * 验证IPv4是否真确
+	 * 
+	 * @param arrayIP
+	 *            ipv4字符串
+	 * @return true/false
+	 */
+	private boolean checkIPv4(String[] arrayIP) {
+		for (int i = 0; i < arrayIP.length; i++) {
+			String ipv4 = arrayIP[i];
+			// Xi是空
+			// Xi是两位以上时，第一位是零
+			// 以上的场合，IPv4无效
+			if (ipv4.isEmpty() || (ipv4.length() > 1 && "0".equals(ipv4.substring(0, 1)))) {
+				return false;
+			}
+			try {
+				int ipv4Temp = Integer.valueOf(ipv4);
+				if (ipv4Temp > 255) {
+					return false;
+				}
+			} catch (Exception e) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * 验证IPv6是否真确
+	 * 
+	 * @param arrayIP
+	 *            ipv6字符串
+	 * @return true/false
+	 */
+	private boolean checkIPv6(String[] arrayIP) {
+		for (int i = 0; i < arrayIP.length; i++) {
+			String ipv6 = arrayIP[i];
+			// Xi是空
+			// Xi是四位以上时
+			// 以上的场合，IPv6无效
+			if (ipv6.isEmpty() || ipv6.length() > 4) {
+				return false;
+			}
+
+			if (!ipv6.matches("[0-9a-fA-F]+")) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
---
+++ src/com/LeetCode/solution/Solution0699.java
@@ -0,0 +1,59 @@
+package com.LeetCode.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author Administrator 在二维平面上�? x 轴上，放置着�?些方块�?? 给你�?个二维整数数�? positions ，其�?
+ *         positions[i] = [lefti, sideLengthi] 表示：第 i 个方块边长为 sideLengthi ，其左侧边与
+ *         x 轴上坐标点 lefti 对齐�? 每个方块都从�?个比目前�?有的落地方块更高的高度掉落�?�下。方块沿 y 轴负方向下落，直到着陆到
+ *         另一个正方形的顶�? 或�?�是 x 轴上 。一个方块仅仅是擦过另一个方块的左侧边或右侧边不算着陆�?�一旦着陆，它就会固定在原地，无法移动�??
+ *         在每个方块掉落后，你必须记录目前�?有已经落稳的 方块堆叠的最高高�? �? 返回�?个整数数�? ans ，其�? ans[i] 表示在第 i
+ *         块方块掉落后堆叠的最高高度�?? 来源：力扣（LeetCode�?
+ *         链接：https://leetcode.cn/problems/falling-squares
+ *         著作权归领扣网络�?有�?�商业转载请联系官方授权，非商业转载请注明出处�??
+ */
+public class Solution0699 {
+
+	public List<Integer> fallingSquares(int[][] positions) {
+		List<Integer> res = new ArrayList<>();
+
+		int[][] tempArr = new int[10000][10000];
+		int max = 0;
+
+		for (int i = 0; i < positions.length; i++) {
+			int left = positions[i][0];
+			int sideLength = positions[i][1];
+			int row = getRow(tempArr, max, left, sideLength);
+			int col = left;
+			editArr(tempArr, row, col, sideLength);
+
+			if (row + sideLength > max) {
+				max = row + sideLength;
+			}
+			res.add(max);
+		}
+
+		return res;
+	}
+
+	private int getRow(int[][] arr, int maxRow, int left, int sideLength) {
+		for (int i = maxRow; i > 0; i--) {
+			for (int j = left; j < left + sideLength; j++) {
+				if (arr[i][j] == 1) {
+					return i;
+				}
+			}
+		}
+		return 0;
+	}
+
+	private void editArr(int[][] arr, int row, int col, int sideLength) {
+		for (int i = 0; i <= sideLength; i++) {
+			for (int j = 0; j <= sideLength; j++) {
+				arr[row + i][col + j] = 1;
+			}
+		}
+	}
+}
